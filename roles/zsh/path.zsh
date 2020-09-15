b6330a31528378200cf74c282113b98c571aee6d
Dinko Lubina <lubinadinko@gmail.com>
2020-09-15 13:51:33 +0200

    Add .local/bin to path

--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -3,3 +3,5 @@ export PATH=$PATH:/$HOME/.dotfiles/bin
 export PATH=$PATH:/snap/bin:$PATH
 export PATH=$PATH:/home/dinko/opt/diff-so-fancy
 export PATH=/home/dinko/.poetry/bin:$PATH
+# Used for making cookiecutter discoverable
+export PATH=$HOME/.local/bin:$PATH
