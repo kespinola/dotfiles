3af73b55cf3eb295ac93b5400f055f81ef1d38a1
Dinko Lubina <lubinadinko@gmail.com>
2021-10-25 21:14:10 +0200

    Add alias to move workspace to other monitor

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -40,3 +40,5 @@ alias top="sudo htop"
 alias shortcuts="bat /home/dinko/.dotfiles/roles/zsh/keyboard_shortcuts.zsh"
 
 export gitf () {search_result=$(git aliases | fzf | awk '{print substr($1, 1, length($1)-1)}'); if [[ -z $search_result ]]; then :; else eval 'git '$search_result; fi}
+
+alias move-ws="sudo i3 move workspace to output"
