95e45a849fc0c20e5abbead04d6078bf03e5da38
Dinko Lubina <dinko.lubina@tink.com>
2022-01-31 09:34:14 +0100

    multiple fixes to the settings

--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -2,6 +2,6 @@ export PATH=${PATH}:$ZSH/bin
 export PATH=$PATH:/$HOME/.dotfiles/bin
 export PATH=$PATH:/snap/bin:$PATH
 export PATH=$PATH:/home/dinko/opt/diff-so-fancy
-export PATH=/home/dinko/.poetry/bin:$PATH
 # Used for making cookiecutter discoverable
 export PATH=$HOME/.local/bin:$PATH
+
